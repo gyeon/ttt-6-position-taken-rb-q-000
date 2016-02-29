d380b313594120d7e9399666875871f112bcf216
gyeon <gyeon19@gmail.com>
2016-02-29 20:50:30 +0000

    nitrous-backup at Mon Feb 29 20:50:30 UTC 2016

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,14 @@
+board = [
+  " ", " ", " ",
+  " ", " ", " ",
+  " ", " ", " "
+  ]
+def position_taken?(board, position)
+  if board, position[0] != " "
+    returns "false"
+  end
+end
+
+
 # code your #position_taken? method here!
+
